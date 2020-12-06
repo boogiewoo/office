435e6e4ec80e18994dd0459133173f262e874ff6
boogiewoo <boogiewoo@users.noreply.github.com>
2020-12-06 14:28:22 +0100

    Add files via upload

---
+++ hardware_transparent_bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="hardware_transparent_bg" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="../tiles/old_computer_hardware/spritesheets/hardware_transparent_bg.png" width="512" height="320"/>
+</tileset>
